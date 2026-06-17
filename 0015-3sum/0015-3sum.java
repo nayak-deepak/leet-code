c812458db18a34f8b93123fdbca2e8da5e9a3242
Deepak Nayak <deepaknayak55260@gmail.com>
2026-06-17 08:33:13 +0530

    Time: 36 ms (30.16%), Space: 59.4 MB (32.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> triplet = new ArrayList<>();
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            
-            int j = i + 1;
-            int k = nums.length - 1;
 
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
+            int base = i;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[base] + nums[left] + nums[right];
+                if (sum == 0) {
+                    triplet.add(List.of(nums[base], nums[left], nums[right]));
 
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
                     }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
                 }
             }
         }
-        return res;        
+
+        return triplet;
     }
 }
