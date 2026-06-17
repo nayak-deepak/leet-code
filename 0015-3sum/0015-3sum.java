2b7d556028dcef487fabf23725fced5f37d47160
Deepak Nayak <deepaknayak55260@gmail.com>
2026-06-17 08:26:01 +0530

    Time: 35 ms (43.23%), Space: 59.2 MB (59.28%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,32 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triplet = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
-        for (int i = 0; i < n && nums[i] <= 0; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+
+        for (int i = 0; i < nums.length - 2; i++) {
+             if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
-            twoSum(nums, result, i + 1, n - 1, 0 - nums[i]);
-        }
-        return result;
-    }
-
-    private void twoSum(int[] nums, List<List<Integer>> result, int left, int right, int target) {
-        while (left < right) {
-            if (nums[left] + nums[right] > target)
-                right--;
-            else if (nums[left] + nums[right] < target)
-                left++;
-            else {
-                result.add(Arrays.asList(-target, nums[left], nums[right]));
-                left++;
-                while (left < right && nums[left] == nums[left - 1]) {
+            
+            int base = i;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[base] + nums[left] + nums[right];
+                if (sum == 0) {
+                    triplet.add(Arrays.asList(nums[base], nums[left], nums[right]));
+                    left++;
+                    while(nums[left] == nums[left - 1] && left < right){
+                        left++;
+                    }
+                } else if (sum > 0) {
+                    right--;
+                } else {
                     left++;
                 }
             }
         }
+
+        return triplet;
     }
 }
