08a2c35e826bb2a240fc100ea48d922df8cc3a7f
Deepak Nayak <deepaknayak55260@gmail.com>
2026-06-17 08:38:43 +0530

    Time: 36 ms (30.16%), Space: 59.4 MB (24.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,7 +4,6 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
